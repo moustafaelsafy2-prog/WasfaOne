b238ceb7fdfa72b7c67d88030d0d7a22fe7d3f11
moustafaelsafy2-prog <moustafa.elsafy2@gmail.com>
2025-09-30 02:18:09 +0400

    Delete public/login.js

--- public/login.js
+++
@@ -1,5 +0,0 @@
-const r = await fetch("/.netlify/functions/login", {
-  method:"POST",
-  headers:{"Content-Type":"application/json"},
-  body: JSON.stringify({ email, password, device_fingerprint: fingerprint })
-});
